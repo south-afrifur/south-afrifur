76dfb8456a67689596b64a364ba10cef6a37e24a
scarletjack <flash.jjvv@gmail.com>
2025-10-15 21:03:31 +0200

    Not Found and scroll restoration

---
+++ src/components/NotFound.tsx
@@ -0,0 +1,29 @@
+import { Container, Image, Stack, Title } from '@mantine/core';
+
+const NotFound = () => {
+  return (
+    <Stack w="100%" h="100%" gap={70} justify="center" pt={100} flex={1}>
+      <Title size={100} ta="center" flex={0}>
+        404
+      </Title>
+      <Container maw={600} flex={0}>
+        <Title ta="center" order={3}>
+          We couldn't find what you're looking for, but it's probably in here somewhere!
+        </Title>
+      </Container>
+      <Stack
+        h="100%"
+        flex={1}
+        justify="end"
+        mb={{
+          md: -50,
+          base: -110,
+        }}
+      >
+        <Image src="/dumbass.png" alt="Hero Image" w={200} mx="auto" />
+      </Stack>
+    </Stack>
+  );
+};
+
+export { NotFound };
--- src/main.tsx
+++ src/main.tsx
@@ -5,10 +5,12 @@ import { createTheme, MantineProvider, type MantineColorsTuple } from '@mantine/
 // Import the generated route tree
 import { routeTree } from './routeTree.gen';
 
-import '@mantine/core/styles.css';
+import '@mantine/core/styles.layer.css';
+
+import { NotFound } from './components/NotFound';
 
 // Create a new router instance
-const router = createRouter({ routeTree });
+const router = createRouter({ routeTree, defaultNotFoundComponent: NotFound });
 
 // Register the router instance for type safety
 declare module '@tanstack/react-router' {
@@ -43,7 +45,7 @@ if (!rootElement.innerHTML) {
   root.render(
     <StrictMode>
       <MantineProvider defaultColorScheme="dark" theme={theme}>
-        <RouterProvider router={router} />
+        <RouterProvider router={router} scrollRestoration />
       </MantineProvider>
     </StrictMode>
   );
