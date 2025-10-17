7d6a5961704fddd6ca87ecbfeb117df944e31f80
scarletjack <flash.jjvv@gmail.com>
2025-10-17 13:35:41 +0200

    Disabled socials in case unused

--- src/utils/social-links.tsx
+++ src/utils/social-links.tsx
@@ -1,35 +1,54 @@
 import {
   IconBrandBluesky,
+  IconBrandDiscord,
   IconBrandFacebook,
   IconBrandInstagram,
+  IconBrandTelegram,
   IconBrandTiktok,
   IconBrandX,
 } from '@tabler/icons-react';
 
 export const socialLinks = [
   {
-    icon: <IconBrandX size={18} />,
-    link: 'https://x.com/southafrifur',
+    icon: <IconBrandTelegram size={18} />,
+    link: 'https://t.me/+p9JMDIwXZ_8wMmQ0',
+    color: 'blue',
+    disabled: false,
+  },
+  {
+    icon: <IconBrandDiscord size={18} />,
+    link: 'https://discord.gg/sgzdu5T',
     color: 'white',
+    disabled: false,
+  },
+  {
+    icon: <IconBrandX size={18} />,
+    link: 'https://x.com/SouthAfriFurCon',
+    color: 'white',
+    disabled: false,
   },
   {
     icon: <IconBrandInstagram size={18} />,
     link: 'https://www.instagram.com/southafrifur/',
     color: '#E4405F',
+    disabled: true,
   },
   {
     icon: <IconBrandFacebook size={18} />,
     link: 'https://www.facebook.com/southafrifur',
     color: '#1877F2',
+    disabled: true,
   },
   {
     icon: <IconBrandTiktok size={18} />,
     link: 'https://www.tiktok.com/@southafrifur',
     color: '#69C9D0',
+    disabled: true,
   },
   {
     icon: <IconBrandBluesky size={18} />,
     link: 'https://bsky.app/profile/southafrifur.bsky.social',
     color: '#1185FE',
+    disabled: true,
   },
 ];
